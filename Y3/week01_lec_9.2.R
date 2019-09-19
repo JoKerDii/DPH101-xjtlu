6ccb6f8e570c982edff530e6ebe59165dadc6bc7
JoKerDii <di.zhen17@student.xjtlu.edu.cn>
2019-09-19 22:40:43 +0800

    week01

---
+++ Y3/week01_lec_9.2.R
@@ -0,0 +1,63 @@
+# 9.2 DPH101 lec
+# bacis operation
+1+2+3
+1+2*3
+1:50
+c(0,1,2,3,4,5,8)
+c(1,2,3,4) + c(10,20,30,40)
+10:1
+c(1:4) + 1
+1/c(1,2,3,4,5)
+c(1,2,3,4) + c(10,100)
+c(1,2,3,4,5) + c(10,100)
+1+2+# ENV203 is difficult
+3  
+exp(12)
+cos(pi)
+log(x = 64, base = 4)
+# variables
+x <- 1
+y = 2
+3 -> z
+t <- c(x,y,z)
+t
+y <- 4
+t
+b <- c(1:12)
+b
+b[7]
+b[1:6]
+b[c(1,3,8,12)]
+c <- c(4:14)
+c
+c > 7
+c ==7
+c != 7
+cat <- 1; Cat <- 2; cAT <- 3
+# Data structure
+help(array)
+d <- array(c(1:12), dim = c(3,4))
+d
+d[2,2]
+e <- array(c(1:18), dim = c(3,3,2))
+e
+e[1,1,1]
+d[1,]
+d[,1]
+d[,2:4]
+d[c(1,3),]
+student <- c("A","B","C","D")
+id <- c(1313,1314,1315,1316)
+grade <- c(75,65,60,80)
+dph <- data.frame(student,id,grade)
+dph$student
+dph$grade<67
+rnorm
+
+## rule
+# 99.22%
+# 0.9922
+# stat(mean, sd, etc.) : +1
+# variance: +2
+
+
